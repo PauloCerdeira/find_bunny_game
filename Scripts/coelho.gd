10ae68bad5140f21c3156d67ed961b0ce14069ec
Paulo Cerdeira <paulocerdeira1999@gmail.com>
2021-10-19 16:37:25 -0300

    ajustes

--- Scripts/coelho.gd
+++ Scripts/coelho.gd
@@ -4,6 +4,7 @@ var pos_inicial = 0
 export var pos_final = 500
 var subir = true
 export var vel = 1
+var pre_cacos = preload("res://Scenes/cacos.tscn")
 
 func _ready():
 	pos_inicial = position.y
@@ -19,4 +20,13 @@ func _process(_delta):
 		self.position.y += vel * 0.5
 
 func _on_Button_button_down():
+	$"../audio".play()
+	#same as:
+	#get_parent().audio.play()
+	criarCacos()
 	self.queue_free()
+
+func criarCacos():
+	var cacos = pre_cacos.instance()
+	cacos.position = self.position
+	get_parent().add_child(cacos)
